3b3b0dcc4fe5af37e5495a838e032e059e7d3261
Joakim Kjellsson <joakimkjellsson@gmail.com>
2025-04-04 07:40:42 +0200

    add chunking when opening files

--- focitools/functions.py
+++ focitools/functions.py
@@ -1,8 +1,24 @@
-def open_multifile_dataset(files, concat_dim="time_counter"):
+def open_multifile_dataset(files, 
+                           concat_dim='time_counter',
+                           chunks={'time_counter':1,'lat':-1,'lon':-1}):
     """
-    Read a dataset from a list of files. 
+    Read a dataset from a list of files. The list should contain files from the same grid, 
+    e.g. grid_T files etc. 
 
+    Input
+    -----
+    files - List of files, e.g. grid_T files
+    concat_dim (optional) - Dimension to concatenate files over. 
+                            Is time_counter for OpenIFS, NEMO
+                            Is time for ECHAM
+    chunks (optional) - Dictionary with chunks for each dimension
+
+    Output
+    ------
+    ds - xarray.Dataset with data from all files merged into one dataset
+    
     This function will read in parallel and use cftime. 
+    Chunking is applied. By default, no chunking in lon,lat, and size 1 in time. 
     """
 
     import cftime
@@ -11,16 +27,11 @@ def open_multifile_dataset(files, concat_dim="time_counter"):
     # open multi-file data set. We need to use cftime since the normal python calendar stops working after 2300. 
     # also, we rename time variable from time_counter to time to make life easier
     ds = xr.open_mfdataset(files,combine='nested', 
+                           chunks=chunks,
                            concat_dim=concat_dim, use_cftime=True,
                            data_vars='minimal', coords='minimal',
                            compat='override',
                            parallel=True)
     
     return ds
-
-#def load_exp_info(machine, expname):
-#    """
-#    
-#    """
-
     
