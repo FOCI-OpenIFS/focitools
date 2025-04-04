1fc0d867655d0098783f4db0c9c31313116776ee
Joakim Kjellsson <joakimkjellsson@gmail.com>
2025-04-04 07:43:37 +0200

    set default chunks and improve comments in read_nemo

--- focitools/read_nemo.py
+++ focitools/read_nemo.py
@@ -8,21 +8,31 @@ def read_nemo(exp_list, time_list, esm_dir,
     Read output from NEMO
 
     Input
+    -----
     exp_list - List of experiment IDs, e.g. FOCI_GJK029
     time_list - List time slices to read
     esm_dir - Directory to experiments (usually named esm_experiments)
     
     Input (optional)
+    ----------------
     grid - e.g. grid_T, grid_U, icemod, ptrc_T, etc. Default: grid_T
     freq - e.g. 1m, 5d. Default: 1m
     decode_timedelta - Decode time differences (True) or not (False). Default: True
     agrif_prefix - Prefix for AGRIF files, e.g. 1_. Default: empty string
 
+    Output
+    ------
+    ds_all - list with xarray.Dataset for each experiment
+    
     Note: 
+    -----
     Xarray decodes timedelta to nanoseconds, which overflow after approx 300 years 
     (300 years is approx 2^63 ns). 
-    Therefore, it is advisable to set decode_timedelta=False when reading tracer age files
+    Therefore, it is advisable to set decode_timedelta=False 
+    when reading tracer age files (ptrc)
     """
+
+    chunks = {'time_counter':120,'deptht':-1,'y':-1,'x':-1}
     
     # list for all data
     ds_all = []
@@ -34,10 +44,10 @@ def read_nemo(exp_list, time_list, esm_dir,
         else:
             files = '%s/%s/outdata/nemo/%s%s*%s*%s.nc' % (esm_dir,exp,agrif_prefix,exp,freq,grid)
         print(files)
-        
+
         # use function to read multi-file data set
         # Will use cftime, read in parallel, etc. 
-        ds = functions.open_multifile_dataset(files).rename({'time_counter':'time'}).sel(time=time)
+        ds = functions.open_multifile_dataset(files, chunks=chunks).rename({'time_counter':'time'}).sel(time=time)
         
         ds_all.append(ds)
         
@@ -45,6 +55,21 @@ def read_nemo(exp_list, time_list, esm_dir,
 
 
 def read_amoc(exp_list, time_list, esmdir):
+    """
+    Read meridional overturning stream functions computed by nemo_monitoring in FOCI. 
+    This will read the stream functions as well as AMOC at 25N and 45N. 
+
+    Input
+    -----
+    exp_list - List of experiment IDs
+    time_list - List of time slices to read for each experiment
+                e.g. [slice('1990-01-01','2015-01-01')]
+    esmdir - Directory where experiments are stored, e.g. $WORK/esm-experiments
+
+    Output
+    ------
+    ds_all - List of xarray.Dataset containing MOC for each experiment
+    """
     
     derived_list = ['moc','amoc_max_25.000N','amoc_max_45.000N']
     derived_name = ['moc','amoc25','amoc45']
@@ -61,6 +86,7 @@ def read_amoc(exp_list, time_list, esmdir):
             
             # use function to read multi-file data set
             # Will use cftime, read in parallel, etc. 
+            chunks={'time_counter':-1}
             ds = functions.open_multifile_dataset(files).rename({'time_counter':'time'}).sel(time=time)
             
             # For overturning stream functions, add latitude on y coord
@@ -82,7 +108,30 @@ def read_amoc(exp_list, time_list, esmdir):
 
 
 def read_transports(exp_list, time_list, esmdir):
+    """
+    Read transports through straits computed by nemo_monitoring in FOCI. 
+    See the nemo_monitoring.sh script in ESM-Tools (configs/components/nemo/) for description. 
+
+    Note
+    ----
+    The straits are hard coded in this function. This may seem inadvisable, but it is 
+    very possible that fluxes through straits will be done directly by XIOS in the future
+    rather than CDFTOOLS, so this function may become obsolete for NEMO 4.2.x experiments. 
+
+    Input
+    -----
+    exp_list - List of experiment IDs
+    time_list - List of time slices to read for each experiment
+                e.g. [slice('1990-01-01','2015-01-01')]
+    esmdir - Directory where experiments are stored, e.g. $WORK/esm-experiments
+
+    Output
+    ------
+    ds_all - List of xarray.Dataset containing transports (merged) for each experiment
     
+    """
+    
+    # List of all transports. Note: Hard coded! 
     transp_list = ['AFR_AUSTR','AM_AFR','AUS_AA','AUSTR_AM','BAFFIN','BERING',
                    'CAMPBELL','CUBA_FLORIDA','DAVIS','DENMARK_STRAIT','DRAKE',
                    'FLORIDA_BAHAMAS','FRAM','ICELAND_SCOTLAND','ITF',
@@ -138,6 +187,7 @@ def read_psi(exp_list, time_list, esm_dir):
 
     Output
     ds_all - List with all Datasets 
+    
     """
     derived_list = ['psi']
     derived_name = ['psi']
